7267c3483982a64d4ea52d7f592502a6f1744682
Peter Derous <peter.derous@skynet.be>
2016-06-30 12:49:08 +0200

    Eerste Backup

--- src/test/java/com/realdolmen/course/PassengerPersistenceTest.java
+++ src/test/java/com/realdolmen/course/PassengerPersistenceTest.java
@@ -1,6 +1,7 @@
 package com.realdolmen.course;
 
 import com.realdolmen.course.domain.Book;
+import com.realdolmen.course.domain.Passenger;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -13,61 +14,30 @@ import static org.junit.Assert.assertNotNull;
 /**
  * Created by cerseilannister on 30/06/16.
  */
-public class PassengerPersistenceTest {
+public class PassengerPersistenceTest extends DataSetPersistenceTest {
 
     @Test
-    public void bookCanBePersisted() throws Exception {
-        Book book = new Book("Animal Farm", "George Orwell", Book.Genre.thriller);
-        entityManager().persist(book);
-        assertNotNull(book.getId());
+    public void passengerCanBePersisted() throws Exception {
+        Passenger passagier = new Passenger(1L, "socialnumber1", "Peter","Derous",500);
+        entityManager().persist(passagier);
+        assertNotNull(passagier.getId());
+
+
     }
+
 
     @Test(expected = PersistenceException.class)
-    public void bookCanNotBePersistedWithoutTitle() throws Exception {
-        Book book = new Book(null, "John Doe", Book.Genre.thriller);
-        entityManager().persist(book);
+    public void passengerCanNotBePersistedWithoutTitle() throws Exception {
+        Passenger passagier2 = new Passenger(null,"Socialnumber2","John", "Doe", 10);
+        entityManager().persist(passagier2);
     }
 
+
     @Test
-    public void bookCanBeRetrievedById() throws Exception {
-        assertEquals("Nineteen Eighty Four", entityManager().find(Book.class, 1000).getTitle());
+    public void passengerCanBeRetrievedById() throws Exception {
+       assertEquals("Peter", entityManager().find(Passenger.class, 1L).getFirstName() );
     }
+
 }
 
-/*
-package com.realdolmen.course;
 
-import com.realdolmen.course.domain.Book;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.ExpectedException;
-
-import javax.persistence.PersistenceException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-
-public class BookPersistenceTest extends DataSetPersistenceTest {
-    @Rule
-    public ExpectedException expector = ExpectedException.none();
-
-    @Test
-    public void bookCanBePersisted() throws Exception {
-        Book book = new Book("Animal Farm", "George Orwell", Book.Genre.thriller);
-        entityManager().persist(book);
-        assertNotNull(book.getId());
-    }
-
-    @Test(expected = PersistenceException.class)
-    public void bookCanNotBePersistedWithoutTitle() throws Exception {
-        Book book = new Book(null, "John Doe", Book.Genre.thriller);
-        entityManager().persist(book);
-    }
-
-    @Test
-    public void bookCanBeRetrievedById() throws Exception {
-        assertEquals("Nineteen Eighty Four", entityManager().find(Book.class, 1000).getTitle());
-    }
-}
-
- */
